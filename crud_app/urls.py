55253b4ffd0f92105c950e4d0edd357940df59ed
Rodrigo <r.ms01@hotmail.com>
2023-12-06 01:46:04 -0500

    Django Avance 6

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 from . import views
-from .views import mi_vista
+from .views import mi_vista ,listar_citas, detalle_cita, editar_cita, eliminar_cita
 
 urlpatterns = [
     path('', mi_vista, name='mi_vista'),
-    path('registrarCita', views.registrarCita, name='registrarCita'),
     path('client', views.client, name='client'),
     path('contact', views.contact, name='contact'),
     path('index', views.mi_vista, name='index'),
+    path('login', views.login, name='login'),
+
+    
+    #Funciones CRUD
+    path('registrarCita', views.registrarCita, name='registrarCita'),
+    
+    path('listar_citas', views.listar_citas, name='listar_citas'),
+    path('app_crud/<int:id>/', detalle_cita, name='detalle_cita'),
+    #Revisar esta parte
+    path('cita/editar/<int:id>/', editar_cita, name='editar_cita'),
+    path('cita/eliminar/<int:id>/', eliminar_cita, name='eliminar_cita'),
 ]
--- crud_app/views.py
+++ crud_app/views.py
@@ -1,9 +1,12 @@
 # Create your views here.
 # views.py
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cita
+from .forms import CitaForm  # Necesitarás crear un formulario (forms.py) para el modelo Cita
 
+
+#Vistas de las páginas
 def mi_vista(request):
     return render(request, 'index.html')
 
@@ -13,6 +16,10 @@ def contact(request):
 def client(request):
     return render(request, 'client.html')
 
+def login(request):
+    return render(request, 'login.html')
+
+#Funciones CRUD
 def registrarCita(request):
     nombre = request.POST["nombre"]
     telefono = request.POST["telefono"]
@@ -22,4 +29,27 @@ def registrarCita(request):
     cita = Cita(nombre=nombre, telefono=telefono, correo=correo, mensaje=mensaje)
     cita.save()
     return redirect("/crud_app/")
-    
+
+def eliminar_cita(request, id):
+    cita = get_object_or_404(Cita, id=id)
+    cita.delete()
+    return redirect('listar_citas')
+
+def listar_citas(request):
+    citas = Cita.objects.all()
+    return render(request, 'listar_citas.html', {'citas': citas})
+
+def detalle_cita(request, id):
+    cita = get_object_or_404(Cita, id=id)
+    return render(request, 'detalle_cita.html', {'cita': cita})
+
+def editar_cita(request, id):
+    cita = get_object_or_404(Cita, id=id)
+    if request.method == 'POST':
+        form = CitaForm(request.POST, instance=cita)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_citas')
+    else:
+        form = CitaForm(instance=cita)
+    return render(request, 'cita/editar_cita.html', {'form': form})
